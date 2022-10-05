0e614830f045b0382390060b0c0ad36fab27348b
Elio Scaletta <elioscal@hotmail.it>
2022-10-05 15:24:54 +0200

    Migliorie e aggiunte alla base della repository.

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
 //importiamo la libreria express che serve per far funzionare il server
 import express from "express";
 import "express-async-errors";
+import prisma from "./lib/prisma/client";
 
 //creiamo una istanza di express assegnando l'esecuzione ad "app"
 const app = express();
 
 //chiamiamo il server con una chiamata di tipo GET e spediamo una risposta 
-app.get("/iPhones", (request, response) => {
-    response.json([
-        { name: "iPhone XS" },
-        { name: "iPhone 11" },
-        { name: "iPhone 12" },
-        { name: "iPhone 13" },
-        { name: "iPhone 14" },
-    ]);
+app.get("/iPhones", async (request, response) => {
+    const iPhones = await prisma.phones.findMany();
+
+    response.json(iPhones);
 });
 
 export default app;
