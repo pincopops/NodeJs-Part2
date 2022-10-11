a7c9db51946ba80a01c253dc3ad929bbc4dbc6d7
Elio Scaletta <elioscal@hotmail.it>
2022-10-11 10:50:23 +0200

    Add Passport with GitHub authentication strategy.

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import "express-async-errors";
 
 import { validationErrorMiddleware } from "./lib/prisma/middleware/validation";
 import { initCorsMiddleware } from "./lib/prisma/middleware/cors";
+import { initSessionMiddleware } from "./lib/prisma/middleware/session";
+import passport from "passport";
 
 import iphonesRoute from "./routes/iphones";
 
@@ -12,6 +14,10 @@ import iphonesRoute from "./routes/iphones";
 
 //creiamo una istanza di express assegnando l'esecuzione ad "app"
 const app = express();
+
+app.use(initSessionMiddleware());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 
--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ const missinSetting = "Warning: No value set for this environment variable";
 
 const config = {
     "PORT": process.env.PORT || missinSetting,
+    "SESSION_SECRET": process.env.SESSION_SECRET || missinSetting,
 };
 
 export default config;
