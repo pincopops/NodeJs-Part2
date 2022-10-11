dfdbe8bd30697f7175d257f23bebd1c6f2cf7780
Elio Scaletta <elioscal@hotmail.it>
2022-10-11 10:14:44 +0200

    Reorganise middleware.

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
 //importiamo la libreria express che serve per far funzionare il server
 import express from "express";
 import "express-async-errors";
-import cors from "cors";
 
-import { validationErrorMiddleware } from "./lib/prisma/validation";
+
+import { validationErrorMiddleware } from "./lib/prisma/middleware/validation";
+import { initCorsMiddleware } from "./lib/prisma/middleware/cors";
 
 import iphonesRoute from "./routes/iphones";
 
-const corsOptions = {
-    origin: "http://localhost:8080"
-};
+
 
 //creiamo una istanza di express assegnando l'esecuzione ad "app"
 const app = express();
 
 app.use(express.json());
 
-app.use(cors(corsOptions));
+app.use(initCorsMiddleware());
 
 app.use("/iphones", iphonesRoute);
 
